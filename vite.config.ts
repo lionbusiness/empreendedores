80bfb6f6d28624e2e774195f5cd3b3f0c79ad779
lionbusiness <ct.lionbusiness@gmail.com>
2026-09-23 00:52:11 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -25,15 +25,18 @@ export default defineConfig({
         theme_color: '#0E0B08',
         background_color: '#0E0B08',
         display: 'standalone',
-        start_url: '/',
+        start_url: '/empreendedores/',
+        scope: '/empreendedores/',
         icons: [
-          { src: '/icons/icon-192.png', sizes: '192x192', type: 'image/png' },
-          { src: '/icons/icon-512.png', sizes: '512x512', type: 'image/png' },
-          { src: '/icons/icon-512-maskable.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' }
+          { src: '/empreendedores/icons/icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/empreendedores/icons/icon-512.png', sizes: '512x512', type: 'image/png' },
+          { src: '/empreendedores/icons/icon-512-maskable.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' }
         ]
       },
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}']
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+        navigateFallback: '/empreendedores/index.html',
+        cleanupOutdatedCaches: true
       }
     })
   ]
